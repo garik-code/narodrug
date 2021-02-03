67071ee6139ec31f30cdb0df213731c148faa0aa
Your Name <you@example.com>
2021-02-04 02:19:08 +0300

    deploy

--- www/wp-content/uploads/sucuri/sucuri-lastlogins.php
+++ www/wp-content/uploads/sucuri/sucuri-lastlogins.php
@@ -1,2 +1,3 @@
 <?php exit(0); ?>
 {"user_id":1,"user_login":"artem","user_remoteaddr":"176.59.96.92","user_hostname":"176.59.96.92","user_lastlogin":"2021-02-04 00:45:23"}
+{"user_id":1,"user_login":"artem","user_remoteaddr":"176.59.96.92","user_hostname":"176.59.96.92","user_lastlogin":"2021-02-04 02:10:49"}
