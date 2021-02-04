769f83b951308ae3a85a9b932343132229e97ba7
Your Name <you@example.com>
2021-02-04 03:47:38 +0300

    backup

---
+++ www/wp-content/uploads/sucuri/sucuri-failedlogins.php
@@ -0,0 +1,2 @@
+<?php exit(0); ?>
+{"user_login":"admin","attempt_time":1612398586,"remote_addr":"176.59.96.92","user_agent":"Mozilla\/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/88.0.4324.146 Safari\/537.36"}
--- www/wp-content/uploads/sucuri/sucuri-lastlogins.php
+++ www/wp-content/uploads/sucuri/sucuri-lastlogins.php
@@ -3,3 +3,4 @@
 {"user_id":1,"user_login":"artem","user_remoteaddr":"176.59.96.92","user_hostname":"176.59.96.92","user_lastlogin":"2021-02-04 02:10:49"}
 {"user_id":1,"user_login":"artem","user_remoteaddr":"176.59.96.92","user_hostname":"176.59.96.92","user_lastlogin":"2021-02-04 02:26:09"}
 {"user_id":1,"user_login":"artem","user_remoteaddr":"176.59.96.92","user_hostname":"176.59.96.92","user_lastlogin":"2021-02-04 02:45:24"}
+{"user_id":1,"user_login":"artem","user_remoteaddr":"176.59.96.92","user_hostname":"176.59.96.92","user_lastlogin":"2021-02-04 03:29:53"}
