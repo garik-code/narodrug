1f70bac9a4776ff2428e82029db8b97f956ae675
Your Name <you@example.com>
2021-02-04 03:27:10 +0300

    process

--- www/wp-content/uploads/sucuri/sucuri-hookdata.php
+++ www/wp-content/uploads/sucuri/sucuri-hookdata.php
@@ -1,6 +1,6 @@
 <?php
 // datastore=hookdata;
 // created_on=1612395485;
-// updated_on=1612395485;
+// updated_on=1612398275;
 exit(0);
 ?>
--- www/wp-content/uploads/sucuri/sucuri-lastlogins.php
+++ www/wp-content/uploads/sucuri/sucuri-lastlogins.php
@@ -2,3 +2,4 @@
 {"user_id":1,"user_login":"artem","user_remoteaddr":"176.59.96.92","user_hostname":"176.59.96.92","user_lastlogin":"2021-02-04 00:45:23"}
 {"user_id":1,"user_login":"artem","user_remoteaddr":"176.59.96.92","user_hostname":"176.59.96.92","user_lastlogin":"2021-02-04 02:10:49"}
 {"user_id":1,"user_login":"artem","user_remoteaddr":"176.59.96.92","user_hostname":"176.59.96.92","user_lastlogin":"2021-02-04 02:26:09"}
+{"user_id":1,"user_login":"artem","user_remoteaddr":"176.59.96.92","user_hostname":"176.59.96.92","user_lastlogin":"2021-02-04 02:45:24"}
